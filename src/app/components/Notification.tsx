70fad14e2e0be20dd1fbba21b4215c25a2c238f6
Galleria-Art <evanlindsay06@gmail.com>
2025-01-08 19:37:13 +0000

    KILL YOURSELFFF

--- src/app/components/Notification.tsx
+++ src/app/components/Notification.tsx
@@ -4,9 +4,12 @@ import React from 'react';
 
 interface NotificationProps {
   message: string;
+  isVisible: boolean;
 }
 
-export const Notification: React.FC<NotificationProps> = ({ message }) => {
+export const Notification: React.FC<NotificationProps> = ({ message, isVisible }) => {
+  if (!isVisible) return null;
+  
   return (
     <div className="fixed bottom-4 left-4 bg-white rounded-lg shadow-lg p-4 border border-gray-200">
       <div className="flex items-center space-x-2">
